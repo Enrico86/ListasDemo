858cbe9012934bad49a1fddc20f391a7bfbf4539
Enrico86 <64355652+Enrico86@users.noreply.github.com>
2020-05-17 10:42:45 +0200

    Creado el Model de nuestra App

---
+++ ListasDemo/ListasDemo/Model/Contactos.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListasDemo.Model
+{
+    public class Contactos
+    {
+        private string _firstName;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        private string Phone;
+
+        public string _phone
+        {
+            get { return Phone; }
+            set { Phone = value; }
+        }
+
+        private string Email;
+
+        public string _email
+        {
+            get { return Email; }
+            set { Email = value; }
+        }
+
+
+    }
+}
+
---
+++ ListasDemo/ListasDemo/Model/ContactosRepository.cs
@@ -0,0 +1,24 @@
+﻿using Foundation.ObjectHydrator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListasDemo.Model
+{
+    public class ContactosRepository
+    {
+        public IList<Contactos> Contactos {get; set;}
+        public ContactosRepository()
+        {
+            Hydrator<Contactos> _contactosHydrator = new Hydrator<Contactos>();
+            Contactos = _contactosHydrator.GetList(50);
+
+        }
+
+        public IList<Contactos> GetAll()
+        {
+            return Contactos;
+        }
+
+    }
+}
