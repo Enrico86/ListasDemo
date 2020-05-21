dbd819ea59506d69c0cd1f4e7b42f9c42f985c52
Enrico86 <64355652+Enrico86@users.noreply.github.com>
2020-05-21 19:25:38 +0200

    Establecer nuestro Database como repositorio de nuestra App

--- ListasDemo/ListasDemo/Helpers/Grouping.cs
+++ ListasDemo/ListasDemo/Helpers/Grouping.cs
@@ -6,12 +6,12 @@ using System.Text;
 namespace ListasDemo.Helpers
 {
     public class Grouping<K,T>:ObservableCollection<T>
-        //L estamos diciendo que esta clase tiene que ser inizializada con dos valores, un valor tipo K y
+        //Le estamos diciendo que esta clase tiene que ser inizializada con dos valores, un valor tipo K y
         //otro tipo T. Además hereda de ObservableCollection.
         //La idea de esta clase es que el primer parametro sea la llave por la cual vamos a realizar el agrupamiento
         //en una lista. Si por ejemplo quiero agrupar según la primera letra del FirstName, todas las primeras letras
         //del FirstName de cada elemento de mi lista sería nuestra K.
-        //El parametro T representa el tipo de dato de cada uno de los elemntos de nuestra lista. La ObservableCXollection
+        //El parametro T representa el tipo de dato de cada uno de los elemntos de nuestra lista. La ObservableCollection
         //tiene entonces que ser de tipo T, del tipo de dato de la lista.
     {
         public K Key { get; }
--- ListasDemo/ListasDemo/Model/ContactosRepository.cs
+++ ListasDemo/ListasDemo/Model/ContactosRepository.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ListasDemo.Model
 {
@@ -13,8 +14,19 @@ namespace ListasDemo.Model
         public IList<Contactos> Contacto {get; set;}
         public ContactosRepository()
         {
-            Hydrator<Contactos> _contactoHydrator = new Hydrator<Contactos>();
-            Contacto = _contactoHydrator.GetList(50);
+            //Hydrator<Contactos> _contactoHydrator = new Hydrator<Contactos>();
+            //Contacto = _contactoHydrator.GetList(50);
+
+            //En el constructor de la clase ContactosRepository utilizábamos el Hydrator para generar automaticamente x objetos de tipo 
+            //Contactos. Pero ahora que ya tenemos acceso a nuestro database, ya no queremos coger una lista que se genere automaticamente 
+            //con el Hidrator sino queremos que coja la que tenemos en nuestro database.
+            //Dado que estamos dentro de un contructor y no está permitido poner directamente un Async, vamos a poner la tarea Task que habíamos 
+            //desarrollado en la clase COntactosDataBase, para que coja la info de nuestro database y la ponga en una lista.
+            Task.Run(async () =>
+           Contacto = await App.Database.GetItemsAsync()).Wait();
+            //Accedemos a la propiedad Database a través de la clase App que es donde la habíamos declarado. Y como Database es una propiedad de 
+            //tipo ContactosDataBase podemos invocar la tarea GetItemsAsync que había creado en dicha clase.
+
         }
         public IList<Contactos> GetAll()
         {
@@ -30,12 +42,21 @@ namespace ListasDemo.Model
         }
 
         public ObservableCollection<Grouping<string, Contactos>> GetAllGrouped()
+        //Aquí habíamos creado un método que nos devolvía el conjunto de información dentro de nuestro repositorio (que antes era
+        //generado automaticamente por el hydrator) agrupados según un determinado criterio
         {
-            var sorted = from f in Contacto
+            //Pero como ahora ya no se genera automaticamente, cuando ejecutemos nuestra aplicación e intente agrupar los elementos en 
+            //el database, éste estará vacío y este método nos daría un error. Para resolver este problema tenemos que hacer lo siguiente:
+            IEnumerable<Grouping<string, Contactos>> sorted = new Grouping<string, Contactos>[0];
+            if (Contacto!=null)
+            { 
+
+            sorted = from f in Contacto
                          orderby f.FirstName
                          group f by f.FirstName[0].ToString()
                          into theGroup
                          select new Grouping<string, Contactos>(theGroup.Key, theGroup);
+            }
             return new ObservableCollection<Grouping<string, Contactos>>(sorted);
            
         }
